986226c79f6589a20b92db9f3f5bbab0c5f46fe5
Kuber-Pathak <kuber.211417@ncit.edu.np>
2023-06-28 21:25:15 +0545

    modified

---
+++ Kuber/index.js
@@ -0,0 +1,36 @@
+//For Opening Menu
+
+// document.querySelector(".cross").style.display = "none";
+document.querySelector(".ham").addEventListener("click", () => {
+  document.querySelector(".sidebar2").classList.toggle("sidebargo");
+  if (document.querySelector(".sidebar2").classList.contains("sidebargo")) {
+    document.querySelector(".sidebar").style.display = "none";
+    document.querySelector(".sidebar2").style.display = "inline";
+  } else {
+    document.querySelector(".sidebar").style.display = "none";
+    document.querySelector(".cross").style.display = "inline";
+    // document.querySelector(".sidebar2").style.display = "block";
+  }
+});
+// document.querySelector(".first-item").addEventListener("click", () => {
+//   document.querySelector(".sidebar2").classList.toggle("sidebargo");
+//   if (document.querySelector(".sidebar2").classList.contains("sidebargo")) {
+//     document.querySelector(".sidebar2").style.display = "none";
+//     document.querySelector(".ham2").style.display = "inline";
+//     document.querySelector(".cross").style.display = "inline";
+//   } else {
+//     document.querySelector(".sidebar2").style.display = "inline";
+//     document.querySelector(".cross").style.display = "inline";
+//   }
+// });
+
+// For exiting Menu
+document.querySelector(".cross").addEventListener("click", () => {
+  document.querySelector(".sidebar2").classList.toggle("sidebargo");
+  if (document.querySelector(".sidebar2").classList.contains("sidebargo")) {
+    document.querySelector(".sidebar").style.display = "inline";
+  } else {
+    document.querySelector(".sidebar2").style.display = "none";
+    // document.querySelector(".sidebar").style.display = "inline";
+  }
+});
