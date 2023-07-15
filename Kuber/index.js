8e80db2439c9cf9e24b18277b4e9fe5617074496
Kuber-Pathak <kuber.211417@ncit.edu.np>
2023-07-15 15:29:54 +0545

    javascript added

--- Kuber/index.js
+++ Kuber/index.js
@@ -1,36 +1,59 @@
-//For Opening Menu
-
-// document.querySelector(".cross").style.display = "none";
-document.querySelector(".ham").addEventListener("click", () => {
-  document.querySelector(".sidebar2").classList.toggle("sidebargo");
-  if (document.querySelector(".sidebar2").classList.contains("sidebargo")) {
-    document.querySelector(".sidebar").style.display = "none";
-    document.querySelector(".sidebar2").style.display = "inline";
-  } else {
-    document.querySelector(".sidebar").style.display = "none";
-    document.querySelector(".cross").style.display = "inline";
-    // document.querySelector(".sidebar2").style.display = "block";
-  }
+window.addEventListener("scroll", function () {
+  var header_right = document.querySelector(".top");
+  var header_logo = document.querySelector(".main");
+  header_right.classList.toggle("sticky", window.scrollY > 0);
+  header_logo.classList.toggle("sticky", window.scrollY > 0);
 });
-// document.querySelector(".first-item").addEventListener("click", () => {
-//   document.querySelector(".sidebar2").classList.toggle("sidebargo");
-//   if (document.querySelector(".sidebar2").classList.contains("sidebargo")) {
-//     document.querySelector(".sidebar2").style.display = "none";
-//     document.querySelector(".ham2").style.display = "inline";
-//     document.querySelector(".cross").style.display = "inline";
-//   } else {
-//     document.querySelector(".sidebar2").style.display = "inline";
-//     document.querySelector(".cross").style.display = "inline";
-//   }
-// });
 
-// For exiting Menu
+document.querySelector(".cross").style.display = "none";
+document.querySelector(".ham").addEventListener("click", () => {
+  var sidebar = document.querySelector(".sidebar");
+  var list = document.querySelector(".sidebar-list");
+  sidebar.classList.toggle("sidebar_expand");
+  list.classList.toggle("list");
+  document.querySelector(".icons").classList.toggle("display");
+  document.querySelector(".cross").style.display = "block";
+  document.querySelector(".ham").style.display = "none";
+  document.querySelector(".search-bar").style.display = "none";
+  document.querySelector(".ham-bottom").style.display = "none";
+  document.querySelector(".cross-bottom").style.display = "block";
+});
+
 document.querySelector(".cross").addEventListener("click", () => {
-  document.querySelector(".sidebar2").classList.toggle("sidebargo");
-  if (document.querySelector(".sidebar2").classList.contains("sidebargo")) {
-    document.querySelector(".sidebar").style.display = "inline";
-  } else {
-    document.querySelector(".sidebar2").style.display = "none";
-    // document.querySelector(".sidebar").style.display = "inline";
-  }
+  var sidebar = document.querySelector(".sidebar");
+  sidebar.classList.toggle("sidebar_expand");
+  var list = document.querySelector(".sidebar-list");
+  list.classList.toggle("list");
+  document.querySelector(".icons").classList.toggle("display");
+  document.querySelector(".cross").style.display = "none";
+  document.querySelector(".ham").style.display = "block";
+  document.querySelector(".search-bar").style.display = "block";
+  document.querySelector(".cross-bottom").style.display = "none";
+  document.querySelector(".ham-bottom").style.display = "block";
+});
+
+document.querySelector(".ham-bottom").addEventListener("click", () => {
+  var sidebar = document.querySelector(".sidebar");
+  var list = document.querySelector(".sidebar-list");
+  sidebar.classList.toggle("sidebar_expand");
+  list.classList.toggle("list");
+  document.querySelector(".icons").classList.toggle("display");
+  document.querySelector(".cross").style.display = "block";
+  document.querySelector(".ham").style.display = "none";
+  document.querySelector(".search-bar").style.display = "none";
+  document.querySelector(".ham-bottom").style.display = "none";
+  document.querySelector(".cross-bottom").style.display = "block";
+});
+
+document.querySelector(".cross-bottom").addEventListener("click", () => {
+  var sidebar = document.querySelector(".sidebar");
+  sidebar.classList.toggle("sidebar_expand");
+  var list = document.querySelector(".sidebar-list");
+  list.classList.toggle("list");
+  document.querySelector(".icons").classList.toggle("display");
+  document.querySelector(".cross").style.display = "none";
+  document.querySelector(".ham").style.display = "block";
+  document.querySelector(".search-bar").style.display = "block";
+  document.querySelector(".cross-bottom").style.display = "none";
+  document.querySelector(".ham-bottom").style.display = "block";
 });
